cf74457b727087c4961cd95739681570491dbc98
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-10-20 22:01:26 -0400

    Update per blizzy78/ksp_toolbar#39 to prevent NotSupportedException. …

--- buildRelease.bat
+++ buildRelease.bat
@@ -51,7 +51,8 @@ rem xcopy src\Textures\CI*.* %HOMEDIR%\install\Gamedata\CraftImport\Textures /y
 xcopy src\Textures\CI-*.png   %HOMEDIR%\install\GameData\CraftImport\Textures /Y
 xcopy src\Textures\colorpicker_texture.jpg %HOMEDIR%\install\GameData\CraftImport\Textures /Y
 copy bin\ReleaseExport\CraftImport.dll %HOMEDIR%\install\Gamedata\CraftImport\Plugins
-copy  craftimport.version %HOMEDIR%\install\Gamedata\CraftImport
+copy craftimport.version %HOMEDIR%\install\Gamedata\CraftImport
+copy ..\MiniAVC.dll %HOMEDIR%\install\Gamedata\CraftImport
 copy README.md %HOMEDIR%\install\Gamedata\CraftImport
 copy ChangeLog.txt %HOMEDIR%\install\Gamedata\CraftImport
 pause
