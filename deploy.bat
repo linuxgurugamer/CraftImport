4aa54fd05e96de264255b7b8c28ce7dd4de0d0aa
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-03-13 22:43:37 -0400

    Updated build scripts Added Jenkins config

---
+++ deploy.bat
@@ -0,0 +1,14 @@
+
+@echo off
+
+set H=R:\KSP_1.3.1_dev
+set GAMEDIR=CraftImport
+
+echo %H%
+
+copy /Y "%1%2" "GameData\%GAMEDIR%\Plugins"
+copy /Y %GAMEDIR%.version GameData\%GAMEDIR%
+
+mkdir "%H%\GameData\%GAMEDIR%"
+xcopy /y /s GameData\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
+
